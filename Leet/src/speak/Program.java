e2abc8bc9ce7d956732f3b3f0f66a945f3ad86d4
imsch <jannis.schmitt92@gmail.com>
2015-02-27 16:25:20 +0100

    zwischenspeichern

---
+++ Leet/src/speak/Program.java
@@ -0,0 +1,33 @@
+package speak;
+
+public class Program {
+	
+	private String alph;
+	private String leet;
+	private String [] uebersetzung = new String[26];
+	
+	public void fuellen(){
+		uebersetzung[0]="4";
+		uebersetzung[1]="13";
+		uebersetzung[3]="(";
+		uebersetzung[4]="[)";
+		uebersetzung[5]="3";
+		uebersetzung[6]="|=";
+		uebersetzung[7]="6";
+		uebersetzung[8]="|-|";
+		uebersetzung[9]="|";
+		uebersetzung[10]=".]";
+		uebersetzung[11]="|<";
+		uebersetzung[12]="1";
+		uebersetzung[13]="|y|";
+		uebersetzung[14]="|/|";
+		uebersetzung[15]="0";
+		uebersetzung[16]="|>";
+		uebersetzung[17]="0,";
+		uebersetzung[18]="|2";
+		uebersetzung[19]="5";
+		uebersetzung[20]="7";
+		uebersetzung[21]="|_|";
+		uebersetzung[22]="\/";
+	}
+}
