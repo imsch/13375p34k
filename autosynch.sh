8bb5c8e582f9a2e2655e2200cc07f120a36872e9
imsch <imsch@users.noreply.github.com>
2015-02-27 19:24:52 +0100

    Delete autosynch.sh

--- autosynch.sh
+++
@@ -1,9 +0,0 @@
-#/bin/bash
-#einmal ausführen zum Pushen des Javaprojektordners
-
-read -p "Versionsnummer: " vers
-	mkdir ./Leet_$vers
-	cp ./Leet/* -r ./Leet_$vers/
-	git add *
-	git commit -m "Aktualisierung"
-	git push -u origin master
